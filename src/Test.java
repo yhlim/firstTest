6af933ee3fd3110ccb20165fb49ceafed96c8323
yhlim <yhlim@andwise.com>
2012-08-09 18:07:39 +0900

    Test Project Version

---
+++ src/Test.java
@@ -0,0 +1,18 @@
+
+
+public class Test {
+
+	public  static void main(String[] args){
+		System.out.println("Hello git!!");
+		System.out.println("Hello git1");
+		System.out.println("Hello yoon");
+		test();
+	}
+	
+	public static void test(){
+		System.out.println("Hello test");
+		System.out.println("Hello test1");
+		System.out.println("Hello test2");
+		System.out.println("Hello test3");
+	}
+}
