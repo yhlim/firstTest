a3e1ff2d1876ed6ece4820347a10750713548ca1
yhlim <yhlim@andwise.com>
2012-08-17 17:46:34 +0900

    Test

--- src/Test.java
+++ src/Test.java
@@ -5,7 +5,7 @@ public class Test {
 	public  static void main(String[] args){
 		System.out.println("Hello git!!");
 		System.out.println("Hello git1");
-		System.out.println("Hello yoon");
+		System.out.println("Hello Lim");
 		test();
 	}
 	
